14b73962316af6674367a77f54ec99dc7ce528c7
Verna Koskinen <darth.kipsu@gmail.com>
2015-05-19 11:18:18 +0300

    Update project.clj

--- project.clj
+++ project.clj
@@ -4,18 +4,18 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/java.jdbc "0.3.6"]
-                 [org.clojure/clojurescript "0.0-3211"]
+                 [org.clojure/java.jdbc "0.3.7"]
+                 [org.clojure/clojurescript "0.0-3291"]
                  [im.chit/purnam "0.5.2"]
-                 [im.chit/gyr "0.3.1"] 
+                 [im.chit/gyr "0.4.0"] 
                  [java-jdbc/dsl "0.1.2"]
-                 [postgresql/postgresql "8.4-702.jdbc4"]
-                 [ring/ring-jetty-adapter "1.2.1"]
+                 [postgresql/postgresql "9.3-1102.jdbc41"]
+                 [ring/ring-jetty-adapter "1.3.2"]
                  [ring/ring-json "0.3.1"]
-                 [org.clojure/data.json "0.2.5"]
-                 [compojure "1.3.0"]
+                 [org.clojure/data.json "0.2.6"]
+                 [compojure "1.3.4"]
                  [hiccup "1.0.5"]]
-  :plugins [[lein-ring "0.9.3"]
+  :plugins [[lein-ring "0.9.4"]
             [lein-cljsbuild "1.0.6"]]
   :cljsbuild {
     :builds [{
