8e1386b05439f5316b780c1b9b94c3efa559388f
Verna Koskinen <darth.kipsu@gmail.com>
2015-05-19 20:47:14 +0300

    add simple navbar

--- src/kipsufi/views/layout.clj
+++ src/kipsufi/views/layout.clj
@@ -8,7 +8,13 @@
      [:title "darth.kipsu.fi"]
      (h/include-css "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css")]
     [:body {:ng-app "kipsufi"}
-     [:div {:class "row"}
+     [:nav {:class "navbar navbar-default"}
+      [:div {:class "container-fluid"}
+       [:ul {:class "nav navbar-nav"}
+        [:li [:a {:ng-href "#/"} "Home"]]
+        [:li [:a {:ng-href "#/algorithms"} "Algorithms"]]
+        [:li [:a {:ng-href "#/datastructures"} "Datastructures"]]]]]
+     [:section
       [:div {:class "col-md-10 col-md-offset-1" :ng-view ""}]]
      (h/include-js "//ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js")
      (h/include-js "//ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular-route.js")
