e3e9b3b38b17846d8971c2d2db1ff6d2743b81dd
Verna Koskinen <darth.kipsu@gmail.com>
2015-05-21 07:54:04 +0300

    add routes to algorithms and datastructures lists

--- src-cljs/angular.cljs
+++ src-cljs/angular.cljs
@@ -9,25 +9,33 @@
 (def.config kipsufi [$routeProvider]
             (-> $routeProvider
               (.when "/" (obj :controller "MainController" :templateUrl "views/list.html"))
+              (.when "/algorithms" (obj :controller "AlgorithmsController" :templateUrl "views/list.html"))
+              (.when "/datastructures" (obj :controller "DatastructuresController" :templateUrl "views/list.html"))
               (.when "/algorithms/:name" (obj :controller "AlgorithmController" :templateUrl "views/show.html"))
               (.when "/datastructures/:name" (obj :controller "DatastructureController" :templateUrl "views/show.html"))
               (.otherwise (obj :redirectTo "/"))))
 
 (def.controller kipsufi.MainController [$scope ApiService]
-                (ApiService.recent $scope))
+                (ApiService.list "/api/recent" $scope))
+
+(def.controller kipsufi.AlgorithmsController [$scope ApiService]
+                (ApiService.list "/api/algorithms" $scope))
+
+(def.controller kipsufi.DatastructuresController [$scope ApiService]
+                (ApiService.list "/api/datastructures" $scope))
 
 (def.controller kipsufi.AlgorithmController [$scope $routeParams ApiService]
                 (ApiService.data-for (str "/api/algorithms/" $routeParams.name) $scope))
 
-(def.controller kipsufi.DatastructureController [$scope $routeParams ct]ApiService$http]
+(def.controller kipsufi.DatastructureController [$scope $routeParams ApiService]
                 (ApiService.data-for (str "/api/datastructures/" $routeParams.name) $scope))
 
 (def.service kipsufi.ApiService [$http]
-             (obj :recent (fn [$scope]
-                            (-> $http
-                              (.get "/api/recent")
-                              (.success (fn [res] (! $scope.data res)))
-                              (.error (fn [] (js/console.log "error")))))
+             (obj :list (fn [path $scope]
+                          (-> $http
+                            (.get path)
+                            (.success (fn [res] (! $scope.data res)))
+                            (.error (fn [] (js/console.log "error")))))
                   :data-for (fn [path $scope]
                               (-> $http
                                 (.get path)
