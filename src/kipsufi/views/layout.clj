9b202832491e410d8fd1f3779cc0b927e7928b7f
Verna Koskinen <darth.kipsu@gmail.com>
2015-05-24 18:41:40 +0300

    add initial version of affix navigation

--- src/kipsufi/views/layout.clj
+++ src/kipsufi/views/layout.clj
@@ -11,14 +11,19 @@
      (h/include-css "//fonts.googleapis.com/css?family=Droid+Sans")
      (h/include-css "css/style.css")]
     [:body {:ng-app "kipsufi"}
-     [:nav {:class "navbar navbar-default"}
-      [:div {:class "container-fluid"}
-       [:ul {:class "nav navbar-nav"}
-        [:li [:a {:ng-href "#/"} "Home"]]
-        [:li [:a {:ng-href "#/algorithms"} "Algorithms"]]
-        [:li [:a {:ng-href "#/datastructures"} "Datastructures"]]]]]
-     [:section
+     [:div {:class "layout-pic"}
+      [:img {:src "images/jolla.jpg"}]]
+     [:div {:style "height: 50px;"}
+      [:nav {:class "navbar navbar-default affix-top" :data-spy "affix" :data-offset-top "200"}
+       [:div {:class "container-fluid"}
+        [:ul {:class "nav navbar-nav"}
+         [:li [:a {:ng-href "#/"} "Home"]]
+         [:li [:a {:ng-href "#/algorithms"} "Algorithms"]]
+         [:li [:a {:ng-href "#/datastructures"} "Datastructures"]]]]]]
+     [:section {:class "content"}
       [:div {:class "col-md-10 col-md-offset-1" :ng-view ""}]]
+     (h/include-js "//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js")
      (h/include-js "//ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js")
      (h/include-js "//ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular-route.js")
+     (h/include-js "//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js")
      (h/include-js "js/script.js")]))
