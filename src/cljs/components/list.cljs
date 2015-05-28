77a4193b234ed0cd652ad6146762c333bd270dc7
Verna Koskinen <darth.kipsu@gmail.com>
2015-05-28 20:28:45 +0300

    list items in main page

--- src/cljs/components/list.cljs
+++ src/cljs/components/list.cljs
@@ -8,14 +8,31 @@
 (defn load-items [url]
   (GET url {:response-format :json
             :keywords? true
-            :handler (fn [data] (reset! items (map :name data)))
+            :handler (fn [data] (reset! items data))
             :error-handler (fn [e] (js/console.log "error"))}))
 
-(defn indexed-list [& children]
-  [:div
-   (map-indexed #(with-meta %2 {:key %1}) children)])
+(defn display-in-p [item]
+  [:p item])
 
-(defn list-from [url]
-  (load-items url)
-  [indexed-list
-   (for [item @items] [:p item])])
+(defn single-item [index item]
+  ^{:key index} [:div.col-md-6
+                 [:div.item-selector
+                  [:div.list-item.head
+                   [:p.item-date (:edited item)]
+                   [:h3 (:name item)]]
+                  [:div.list-item.content
+                   (if (:datastructures item)
+                     [:div.col-md-12
+                      [:p [:strong "Datastructures: "]
+                       [:span (map (fn [item] item) (:datastructures item))]]])
+                   [:div.col-md-12.highlight
+                    [:div.col-md-6
+                     [:p [:strong "Advantages:"]]
+                     (map display-in-p (:advantages item))]
+                    [:div.col-md-6
+                     [:p [:strong "Disadvantages:"]]
+                     (map display-in-p (:advantages item))]]
+                   [:div.col-md-12 (:description item)]]]])
+
+(defn list-items []
+  [:div (map-indexed single-item @items)])
