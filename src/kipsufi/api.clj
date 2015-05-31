4b85104b0566f57ba32b8851c64a99b79ed92449
Verna Koskinen <darth.kipsu@gmail.com>
2015-05-31 10:51:42 +0300

    add documentation to private api functions

--- src/kipsufi/api.clj
+++ src/kipsufi/api.clj
@@ -4,20 +4,28 @@
 
 ; ---- PRIVATE ---- ;
 
-(defn ^:private filter-features [obj selector]
+(defn ^:private filter-features
+  "Return features that match the given selector."
+  [obj selector]
   (map (fn [x] (:f1 x)) (selector (fn [x] (:f2 x)) (:features obj))))
 
-(defn ^:private features->advantage-groups [obj]
+(defn ^:private features->advantage-groups
+  "Divide :features into :advantages and :disadvantages."
+  [obj]
   (dissoc (assoc obj
                  :advantages (filter-features obj filter)
                  :disadvantages (filter-features obj remove))
           :features))
 
-(defn ^:private format-time [obj]
+(defn ^:private format-time
+  "Format sgl timestamp into 12 digit int display format."
+  [obj]
   (assoc obj :created (c/to-long (c/from-sql-time (:created obj)))
              :edited (c/to-long (c/from-sql-time (:edited obj)))))
 
-(defn ^:private with-group [group obj]
+(defn ^:private with-group
+  "Add :group to map."
+  [group obj]
   (assoc obj :group group))
 
 ; ---- PUBLIC ---- ;
--- src/kipsufi/core.clj
+++
@@ -1,6 +0,0 @@
-(ns kipsufi.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
