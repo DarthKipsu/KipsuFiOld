823034e5fcfc9e36a2895943f559d5f4256a9bd2
Verna Koskinen <darth.kipsu@gmail.com>
2015-06-07 15:03:02 +0300

    change the way database queries are made

--- src/kipsufi/database.clj
+++ src/kipsufi/database.clj
@@ -13,77 +13,65 @@
 (defn list-algorithms 
   "Reads all algorithms from db and returns them as lazy seq."
   []
-  (query (select [{:a.algorithm_name :name}
-                  :a.description
-                  :a.created :a.edited
-                  {"json_agg(DISTINCT ad.datastructure_name)" :datastructures}
-                  {"json_agg((af.feature_name, af.advantage))" :features}]
-                 {:algorithms :a}
-                 "NATURAL JOIN algorithms_datastructures ad, algorithms_features af"
-                 "GROUP BY a.algorithm_name, a.description"
-                 (order-by :a.edited))))
+  (query (str "SELECT a.algorithm_name AS name,a.description,a.created,a.edited,"
+              "json_agg(DISTINCT ad.datastructure_name) AS datastructures,"
+              "json_agg((af.feature_name, af.advantage)) AS features "
+              "FROM algorithms a "
+              "NATURAL JOIN algorithms_datastructures ad, algorithms_features af "
+              "GROUP BY a.algorithm_name, a.description "
+              "ORDER BY a.edited DESC")))
 
 (defn list-datastructures 
   "Reads all datastructures from db and returns them as lazy seq."
   []
-  (query (select [{:d.datastructure_name :name}
-                  :d.description
-                  :d.created :d.edited
-                  {"json_agg((df.feature_name, df.advantage))" :features}]
-                 {:datastructures :d}
-                 "NATURAL JOIN algorithms_datastructures ad, datastructures_features df"
-                 "GROUP BY d.datastructure_name, d.description"
-                 (order-by :d.edited))))
+  (query (str "SELECT d.datastructure_name AS name,d.description,d.created,d.edited,"
+              "json_agg((df.feature_name, df.advantage)) AS features "
+              "FROM datastructures d "
+              "NATURAL JOIN algorithms_datastructures ad, datastructures_features df "
+              "GROUP BY d.datastructure_name, d.description "
+              "ORDER BY d.edited DESC")))
 
 (defn list-projects 
   "Reads all projects from db and returns them as lazy seq."
   []
-  (query (select [{:p.project_name :name}
-                  :p.description
-                  :p.launched :p.created :p.edited
-                  {"json_agg(DISTINCT pl.language_name)" :languages}]
-                 {:projects :p}
-                 "NATURAL JOIN projects_languages pl"
-                 "GROUP BY p.project_name, p.description"
-                 (order-by :p.edited))))
+  (query (str "SELECT p.project_name AS name,p.description,p.launched,p.created,p.edited,"
+              "json_agg(DISTINCT pl.language_name) AS languages "
+              "FROM projects p "
+              "NATURAL JOIN projects_languages pl "
+              "GROUP BY p.project_name, p.description "
+              "ORDER BY p.edited DESC")))
 
 (defn read-algorithm
   "Reads a single algorithm fron db and returns it with contents."
   [algorithm]
-  (query (select [{:a.algorithm_name :name}
-                  :a.description
-                  :a.content
-                  :a.created :a.edited
-                  {"json_agg(DISTINCT ad.datastructure_name)" :datastructures}
-                  {"json_agg((af.feature_name, af.advantage))" :features}]
-                 {:algorithms :a}
-                 "NATURAL JOIN algorithms_datastructures ad, algorithms_features af"
-                 (where {:a.algorithm_name algorithm})
-                 "GROUP BY a.algorithm_name, a.description")))
+  (query [(str "SELECT a.algorithm_name AS name,a.description,a.content,a.created,a.edited,"
+               "json_agg(DISTINCT ad.datastructure_name) AS datastructures,"
+               "json_agg((af.feature_name, af.advantage)) AS features "
+               "FROM algorithms a "
+               "NATURAL JOIN algorithms_datastructures ad, algorithms_features af "
+               "WHERE a.algorithm_name = ? "
+               "GROUP BY a.algorithm_name, a.description")
+          algorithm]))
 
 (defn read-datastructure
   "Reads a single datastructure from db and returns it with contents."
   [datastructure]
-  (query (select [{:d.datastructure_name :name}
-                  :d.description
-                  :d.content
-                  :d.created :d.edited
-                  {"json_agg((df.feature_name, df.advantage))" :features}]
-                 {:datastructures :d}
-                 "NATURAL JOIN algorithms_datastructures ad, datastructures_features df"
-                 (where {:d.datastructure_name datastructure})
-                 "GROUP BY d.datastructure_name, d.description")))
+  (query [(str "SELECT d.datastructure_name AS name,d.description,d.content,d.created,d.edited,"
+               "json_agg((df.feature_name, df.advantage)) AS features "
+               "FROM datastructures d "
+               "NATURAL JOIN algorithms_datastructures ad, datastructures_features df "
+               "WHERE d.datastructure_name = ? "
+               "GROUP BY d.datastructure_name, d.description")
+          datastructure]))
 
 (defn read-project 
   "Reads a single project from db and returns it with contents and categories."
   [project]
-  (query (select [{:p.project_name :name}
-                  :p.description
-                  :p.content :p.comments :p.better_done :p.lesson
-                  :p.link_url :p.link_name
-                  :p.launched :p.created :p.edited
-                  {"json_agg(DISTINCT pl.language_name)" :languages}]
-                 {:projects :p}
-                 "NATURAL JOIN projects_languages pl"
-                 (where {:p.project_name project})
-                 "GROUP BY p.project_name, p.description")))
+  (query [(str "SELECT p.project_name AS name,p.description,p.content,p.comments,"
+               "p.better_done,p.lesson,p.link_url,p.link_name,p.launched,p.created,p.edited,"
+               "json_agg(DISTINCT pl.language_name) AS languages "
+               "FROM projects p "
+               "NATURAL JOIN projects_languages pl "
+               "WHERE p.project_name = ? "
+               "GROUP BY p.project_name, p.description")
+          project]))
