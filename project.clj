a6e6c23fa4265a5e4e26e28cb0dfb9780f5e11b1
Verna Koskinen <darth.kipsu@gmail.com>
2015-08-09 12:00:23 +0300

    add kerodon to project.clj

--- project.clj
+++ project.clj
@@ -3,24 +3,25 @@
   :url "http://darth.kipsu.fi"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/java.jdbc "0.3.7"]
+  :dependencies [[clj-time "0.9.0"]
                  [cljs-ajax "0.3.11"]
-                 [java-jdbc/dsl "0.1.2"]
-                 [postgresql/postgresql "9.3-1102.jdbc41"]
-                 [ring/ring-jetty-adapter "1.3.2"]
-                 [ring/ring-json "0.3.1"]
-                 [org.clojure/data.json "0.2.6"]
+                 [com.jquery/jquery "1.9.1"]
                  [compojure "1.3.4"]
                  [hiccup "1.0.5"]
                  [hickory "0.5.4"]
-                 [clj-time "0.9.0"]
-                 [com.jquery/jquery "1.9.1"]]
+                 [java-jdbc/dsl "0.1.2"]
+                 [kerodon "0.6.1"]
+                 [org.clojure/clojure "1.6.0"]
+                 [org.clojure/data.json "0.2.6"]
+                 [org.clojure/java.jdbc "0.3.7"]
+                 [postgresql/postgresql "9.3-1102.jdbc41"]
+                 [ring/ring-jetty-adapter "1.3.2"]
+                 [ring/ring-json "0.3.1"]]
   :main kipsufi.web
-  :plugins [[lein-ring "0.9.4"]
+  :plugins [[com.cemerick/clojurescript.test "0.3.3"]
             [lein-lesscss "1.2"]
             [lein-npm "0.5.0"]
-            [com.cemerick/clojurescript.test "0.3.3"]]
+            [lein-ring "0.9.4"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]]
                    :plugins [[lein-midje "3.1.3"]]}
              :uberjar {:aot :all}}
