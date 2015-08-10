d60be4a82d55c6dd26a1726665a622dede09dd01
Verna Koskinen <darth.kipsu@gmail.com>
2015-08-10 23:21:37 +0300

    add feature tests for articles

---
+++ test/kipsufi/features/articles.clj
@@ -0,0 +1,29 @@
+(ns kipsufi.features.articles
+  (:use [kerodon.core]
+        [kerodon.test]
+        [clojure.test])
+  (:require [kipsufi.web :refer [app db api-routes]]))
+
+(println "Running 7 assertions for articles.")
+
+(deftest display-article-list
+  (-> (session app)
+    (visit "/articles")
+    (has (status? 200)
+         "article list page is found")
+    (within [:h1] (has (text? "Articles")
+         "article list page title is Articles"))
+    (within [:h3] (has (some-text? "Article 1")
+         "article list page displays the first article"))
+    (within [:h3] (has (some-text? "Article 8")
+         "article list page displays the last article"))))
+
+(deftest display-article-list
+  (-> (session app)
+    (visit "/articles/Article%201")
+    (has (status? 200)
+         "single article page is found")
+    (within [:div.intro] (has (text? "Article called Article 1")
+         "single article page displays description of the article"))
+    (within [:div.col-md-12] (has (some-text? "Some super cool article.")
+         "single article page displays the contents of the article"))))
