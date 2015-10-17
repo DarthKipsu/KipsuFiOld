5565f0ffc75072d1a35eb50770343eeef7d880b9
Verna Koskinen <darth.kipsu@gmail.com>
2015-10-17 21:22:04 +0300

    add category images to photography

--- src/kipsufi/views/photo/categories.clj
+++ src/kipsufi/views/photo/categories.clj
@@ -24,9 +24,12 @@
 
 (defn index []
   [:section.photography
-   [:a {:href "/photography/camping"} "Camping"] " "
-   [:a {:href "/photography/travel"} "Travel"] " "
-   [:a {:href "/photography/moments"} "Moments"]])
+   [:a {:href "/photography/camping"}
+    [:img {:src "/images/photography/camping.png"}]] " "
+   [:a {:href "/photography/travel"}
+    [:img {:src "/images/photography/travel.png"}]] " "
+   [:a {:href "/photography/moments"}
+    [:img {:src "/images/photography/moments.png"}]]])
 
 (defn wrapper [content]
   [:section.photography
