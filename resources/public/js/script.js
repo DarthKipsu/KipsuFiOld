51aa82a655164ff8e9ed9d7d0b69c9af081df933
Verna Koskinen <darth.kipsu@gmail.com>
2015-10-17 19:49:45 +0300

    improve thumbnail compression

--- resources/public/js/script.js
+++ resources/public/js/script.js
@@ -38,10 +38,31 @@ function imgNumber(img) {
 }
 
 function adjustThumbWidth(displaying, galleryCount) {
+    $("img.thumb").removeClass("displaying");
+    $("img.thumb:nth-child(" + displaying + ")").addClass("displaying");
     var thumbContainWidth = $("div.thumbnails").width();
     if (thumbContainWidth / 100 < galleryCount) {
-        var compressedWidth = (thumbContainWidth - 100) / galleryCount;
-        $("img.thumb").width(compressedWidth);
-        $("img.thumb:nth-child(" + displaying + ")").width(100);
+        var compressedWidth = (thumbContainWidth - 310) / (galleryCount + 3);
+        if (displaying == 1 || displaying == galleryCount) {
+            compressedWidth = (thumbContainWidth - 210) / (galleryCount + 1);
+        } else if (displaying == 2 || displaying == galleryCount - 1) {
+            compressedWidth = (thumbContainWidth - 310) / galleryCount;
+        } else if (displaying == 3 || displaying == galleryCount - 2) {
+            compressedWidth = (thumbContainWidth - 310) / (galleryCount + 2);
+        }
+        if (compressedWidth > 100 / 3) {
+            compressedWidth = (thumbContainWidth - 110) / (galleryCount - 1);
+            $("img.thumb").width(compressedWidth);
+            $("img.thumb:nth-child(" + displaying + ")").width(100);
+        } else {
+            $("img.thumb").width(compressedWidth);
+            $("img.thumb:nth-child(" + displaying + ")").width(100);
+            $("img.thumb:nth-child(" + (displaying + 1) + ")").width(100);
+            $("img.thumb:nth-child(" + (displaying + 2) + ")").width(compressedWidth * 3);
+            $("img.thumb:nth-child(" + (displaying + 3) + ")").width(compressedWidth * 2);
+            $("img.thumb:nth-child(" + (displaying - 1) + ")").width(100);
+            $("img.thumb:nth-child(" + (displaying - 2) + ")").width(compressedWidth * 3);
+            $("img.thumb:nth-child(" + (displaying - 3) + ")").width(compressedWidth * 2);
+        }
     }
 }
